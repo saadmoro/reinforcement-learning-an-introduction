b527bd3ab1f2bce97b8066148ee5b39aa14d1eac
Saad Moro <saadstats@gmail.com>
2025-10-01 03:19:38 -0500

    Add hashing, state exploration algorithm, state value function updating, exploratory actions

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -5,12 +5,23 @@ import pickle
 BOARD_SIZE = 3
 PRIORITY_PROB = 0.5
 
+
 class State:
 
     def __init__(self):
         self.board = np.zeros((BOARD_SIZE, BOARD_SIZE))
+        self.hash_val = None
         self.winner = None
         self.finish = None
+
+    def hash(self):
+        if self.hash_val == None:
+            self.hash_val = 0
+
+            for i in np.nditer(self.board):
+                self.hash_val = self.hash_val * 3 + i + 1
+
+        return self.hash_val
 
     def is_finished(self):
         
@@ -56,7 +67,10 @@ class State:
         return self.finish
     
     def next_state(self, i, j, token):
+        new_state = State()
+        new_state.board = np.copy(self.board)
         self.board[i, j] = token
+        return new_state
 
     def print_board(self):
         horiz = '----'
@@ -77,6 +91,32 @@ class State:
             print(row)
         print(horiz)
 
+def get_all_states_adjacent(current_state, current_symbol, all_states):
+    for i in range(BOARD_SIZE):
+
+        for j in range(BOARD_SIZE):
+
+            if current_state.board[i][j] == 0:
+                new_state = current_state.next_state(i, j, current_symbol)
+                new_hash = new_state.hash()
+
+                if new_hash not in all_states:
+                    is_finished = new_state.is_finished()
+                    all_states[new_hash] = (new_state, is_finished)
+
+                    if not is_finished:
+                        get_all_states_adjacent(new_state, -current_symbol, all_states)
+
+def get_all_states():
+    current_symbol = 1
+    current_state = State()
+    all_states = dict()
+    all_states[current_state.hash()] = (current_state, current_state.is_finished())
+    get_all_states_adjacent(current_state, current_symbol, all_states)
+    return all_states
+
+ALL_STATES = get_all_states()
+
 
 class Player:
 
@@ -84,16 +124,59 @@ class Player:
         self.estimations = dict()
         self.step_size = step_size
         self.epsilon = epsilon
+        self.states = []
         self.token = 0
         
-    def set_token(self, token):
+    def initialize_estimations(self, token):
         self.token = token
 
+        for hash_val in ALL_STATES:
+            state, is_finished = ALL_STATES[hash_val]
+
+            if is_finished:
+                if state.winner == self.token:
+                    self.estimations[hash_val] = 1.0
+                elif state.winner == 0:
+                    self.estimations[hash_val] = 0.5
+                else:
+                    self.estimations[hash_val] = 0.0
+            else:
+                self.estimations[hash_val] = 0.5
+
+
+    #TODO: CHECK BOOK
+    def update(self):
+        states = [state.hash() for state in self.states]
+        
+        for i in reversed(range(len(states) - 1)):
+            state = states[i]
+            td_error = self.greedy[i] * (
+                self.estimations[states[i + 1]] - self.estimations[state]
+            )
+            self.estimations[state] += self.step_size * td_error
+
+
     def reset(self):
-        pass
+        self.states = []
 
     def act(self):
-        pass
+        state = self.states[-1]
+        next_states = []
+        next_positions = []
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                if state.data[i, j] == 0:
+                    next_positions.append([i, j])
+                    next_states.append(state.next_state(i, j, self.symbol).hash())
+        
+        if np.random.rand() < self.epsilon:
+            action = next_positions[np.random.randint(len(next_positions))]
+            action.append(self.token)
+            self.greedy[-1] = False
+            return action
+        
+        values = []
+        #TODO: HERE NEXT
 
     def save_policy(self):
         pass
@@ -137,9 +220,12 @@ class game_master:
 
             #Return winner
 
+def main():
+    all_states = get_all_states()
 
 
 if __name__ == "__main__":
-    game = State()
-    game.print_board()
+    
+
+    #TODO: pylint
     
